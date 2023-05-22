9bdabbc20f664d37c6b66ccf3fbbb7ea30f116cc
Luna Lopis <lunalopis59@gmail.com>
2023-05-23 01:48:37 +0200

    primo esercizio base

--- js/script.js
+++ js/script.js
@@ -2,4 +2,23 @@
 
 let emails = ['primaemail@gmail.com', 'secondaemail@gmail.com', "terzaemail@gmail.com", "quartaemail@gmail.com"];
 
-console.log(emails);
+let email = prompt('inserire il nome del gioco che vuoi cercare');
+let flag = false;
+for (let i = 0; i < emails.length; i++) {
+
+    if (emails[i] === email) {
+        flag = true;
+    }
+}
+if (flag === true) {
+    console.log('elemento trovato');
+}
+else {
+    console.log('elemento non trovato');
+}
+
+
+
+
+
+
